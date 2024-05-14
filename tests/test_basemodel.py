c5e5d838ac7a4f0445ba25f8812b5070278db5ff
Lex9jar <alexanderedim89@gmail.com>
2024-05-14 16:28:05 +0100

    Test file for BaseModel Task-0

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -12,61 +12,52 @@ class TestBaseModelInstances(unittest.TestCase):
 
     def setUp(self):
         """Creates an instance of <BaseModel> for unittesting."""
-
         self.my_model = BaseModel()
 
     def tearDown(self):
         """TearDown mothed for clearing up memory used by <setUp>
         method.
         """
-
         del self.my_model
 
     def test_objectInstance(self):
         """Checks if an instance declared by <setUp> is a <BaseModel>
         object.
         """
-
         self.assertIsInstance(self.my_model, BaseModel)
 
     def test_idInstance(self):
         """Checks if the attribute <id> of an instance <BaseModel>
         is a string.
         """
-
         self.assertIsInstance(self.my_model.id, str)
 
     def test_created_atInstance(self):
         """Checks if the attribute <created_at> of instance
         <self.my_model> is type <datetime>.
         """
-
         self.assertIsInstance(self.my_model.created_at, datetime)
 
     def test_updated_atInstance(self):
         """Checks if the attribute <updated_at> of instnace <self.my_model> is
         type <datetime>.
         """
-
         self.assertIsInstance(self.my_model.updated_at, datetime)
 
     def test__str__Instance(self):
         """Checks if the return object of <str(self.my_model)> is type str."""
-
         self.assertIsInstance(str(self.my_model), str)
 
     def test_to_dictInstance(self):
         """Checks if the return object of <self.my_model.to_dict()> is type
         <dict>.
         """
-
         self.assertIsInstance(self.my_model.to_dict(), dict)
 
     def test_dictInstances(self):
         """Validates if all the items of <self.my_model.to_dict()> are type
         <str>.
         """
-
         obj_dict = self.my_model.to_dict()
 
         self.assertIsInstance(obj_dict['id'], str)
@@ -81,18 +72,15 @@ class TestDictContents(unittest.TestCase):
     def setUp(self):
         """Set up all neccessary protocols for <TestDictContents> unittesting.
         """
-
         self.my_model = BaseModel()
         self.obj_dict = self.my_model.to_dict()
 
     def tearDown(self):
         """Clean up the memory space used by <setUp> method."""
-
         del self.my_model
 
     def test_validation(self):
         """Validates contents of <self.my_model.to_dict()>."""
-
         self.assertIn('__class__', self.obj_dict)
         self.assertIn('id', self.obj_dict)
         self.assertIn('created_at', self.obj_dict)
@@ -100,14 +88,12 @@ class TestDictContents(unittest.TestCase):
 
     def test_className(self):
         """Validates if the '__class__' attribute was correctly inserted."""
-
         self.assertEqual(self.obj_dict['__class__'], 'BaseModel')
 
     def test_not_overridden_dict(self):
         """Makes sure that <self.my_model.to_dict()> do not overwrite
         <self.my_model.__dict__>.
         """
-
         self.assertNotEqual(self.obj_dict, self.my_model.__dict__)
 
         obj_dict2 = self.my_model.to_dict()
