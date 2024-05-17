ca9b6d804e318ae054e4512390adbf563c2c6cf2
Lex9jar <alexanderedim89@gmail.com>
2024-05-17 14:22:56 +0100

    FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """Define a class <FileStorage>."""
 import json
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 from models.base_model import BaseModel
 
 
