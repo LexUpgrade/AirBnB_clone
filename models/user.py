e03172ff43f334ae4842a9d2d043d98553db6321
Lex9jar <alexanderedim89@gmail.com>
2024-05-17 14:20:38 +0100

    Class <User>.

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class <User."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """A <User> class that inherits from <BaseModel>.
+
+    Class Public Attributes:
+        email (str): ...
+        password (str): ...
+        first_name (str): ...
+        last_name (str): ...
+    """
+
+    email = str()
+    password = str()
+    firs_name = str()
+    last_name = str()
