16f6c12049ee914368ed18213fddda7343e21fb7
Lex9jar <alexanderedim89@gmail.com>
2024-05-20 01:12:33 +0100

    FileStorage Module

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,8 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Serializes instances to JSON file and deserializes JSON file to instances.
+    """Serializes instances to JSON file and deserializes JSON file
+    to instances.
     """
 
     __file_path = "file.json"
